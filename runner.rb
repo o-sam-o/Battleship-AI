19e86f186ba30321e617d4b3b19fd1643578bb2a
Sam Cavenagh <sam@Punk.local>
2012-02-07 16:43:28 +1100

    change file locations

--- runner.rb
+++ runner.rb
@@ -1,12 +1,12 @@
-require File.dirname(__FILE__) + '/ship'
-require File.dirname(__FILE__) + '/board_printer'
-require File.dirname(__FILE__) + '/battleship_game'
+require File.dirname(__FILE__) + '/game/ship'
+require File.dirname(__FILE__) + '/util/board_printer'
+require File.dirname(__FILE__) + '/game/battleship_game'
 require File.dirname(__FILE__) + '/ai/random_ship_placement'
 require File.dirname(__FILE__) + '/ai/basic_battleship_ai'
 require File.dirname(__FILE__) + '/ai/random_seeker_ai'
 require File.dirname(__FILE__) + '/ai/random_ai'
 
-ai_1 = BasicBattshipAI.new
+ai_1 = RandomSeekerAI.new
 ai_2 = RandomAI.new
 
 game = BattleshipGame.new(ai_1, ai_2, 1)
