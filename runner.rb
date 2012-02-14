a380e68b2ce3dafd4cc1b692dbcab704c7601688
Sam Cavenagh <sam.cavenagh@gmail.com>
2012-02-13 21:08:15 -0800

    restructure ai folder

--- runner.rb
+++ runner.rb
@@ -3,8 +3,8 @@ require File.dirname(__FILE__) + '/util/board_printer'
 require File.dirname(__FILE__) + '/game/battleship_game'
 require File.dirname(__FILE__) + '/compare/ai_competitor'
 require File.dirname(__FILE__) + '/compare/ai_comparator'
-require File.dirname(__FILE__) + '/ai/random_ship_placement'
-require File.dirname(__FILE__) + '/ai/ai_helpers'
+require File.dirname(__FILE__) + '/ai/placement/random_ship_placement'
+require File.dirname(__FILE__) + '/ai/helpers/ai_helpers'
 require File.dirname(__FILE__) + '/ai/basic_battleship_ai'
 require File.dirname(__FILE__) + '/ai/random_seeker_ai'
 require File.dirname(__FILE__) + '/ai/random_seeker_ai_old'
@@ -18,7 +18,7 @@ ais << RandomAI.new
 ais << BasicBattshipAI.new
 ais << SmartSeekerAI.new
 
-comparator = AIComparator.new(ais, 100)
+comparator = AIComparator.new(ais, 10)
 comparator.compare_ais
 
 p "Done"
